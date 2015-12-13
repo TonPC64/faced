dc458da0e2eb255b7cc9da7846b12ce2fee61484
kingkong64 <kingkong2103@gmail.com>
2015-12-13 14:03:50 +0700

    firt commit

--- index.js
+++ index.js
@@ -1,20 +1,10 @@
-var express = require('express');
-var app = express();
+var express = require('express')
+var app = express()
 
-app.set('port', (process.env.PORT || 5000));
+app.set('port', (process.env.PORT || 5000))
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static('public'))
 
-// views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
-
-app.get('/', function(request, response) {
-  response.render('pages/index');
-});
-
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
-});
-
-
+var server = app.listen(app.get('port'), function () {
+  console.log('Node app is running on port', app.get('port'))
+})
